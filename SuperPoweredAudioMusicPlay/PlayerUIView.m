e064473cc7cb776149cb43f23fe452a49272727b
岳克奎 <738816656@qq.com>
2016-12-28 21:46:20 +0800

    完善PlayerUIView
    PlayerUIView 上面显示播放功能控件

--- SuperPoweredAudioMusicPlay/PlayerUIView.m
+++ SuperPoweredAudioMusicPlay/PlayerUIView.m
@@ -9,15 +9,96 @@
 #import "PlayerUIView.h"
 
 @implementation PlayerUIView
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+#pragma mark -life cycle -----------------------------------控 制 生 命 周 期 区 域 ---------------------------------
+-(instancetype)initWithCoder:(NSCoder *)aDecoder{
+    if (self = [super initWithCoder:aDecoder]) {
+        //因没实力化，子控件要在from nib 写
+    }
+    return self;
 }
-*/
-
+-(void)awakeFromNib{
+    [super awakeFromNib];
+}
+#pragma mark -evebt response --------------------------------- 事件响应区域 -----------------------------------------
+#pragma mark -音效调节
+/**
+ * @brief:音效调节（弹出音效控制界面）
+ *
+ * @dicussion:
+ *
+ */
 - (IBAction)effectAdjustBtn:(UIButton *)sender {
+    if(self.delegate&&[self.delegate respondsToSelector:@selector(showSoundEffectViewcontrollerWithPlayerUIView:)]){
+        [self.delegate showSoundEffectViewcontrollerWithPlayerUIView:self];
+    }
+}
+#pragma mark - 音乐关闭
+/**
+ * @brief: 音乐关闭
+ *
+ * @dicussion:
+ *
+ */
+- (IBAction)closeMusicClick:(UIButton *)sender {
+    if(self.delegate&&[self.delegate respondsToSelector:@selector(closeMusicPlayandUIWithPlayerUIView:)]){
+        [self.delegate closeMusicPlayandUIWithPlayerUIView:self];
+    }
+}
+#pragma mark -
+/**
+ * @brief:
+ *
+ * @dicussion:
+ *
+ */
+- (IBAction)resumeOrPasueMusicClick:(UIButton *)sender {
+    
+}
+
+#pragma mark -音乐音量调节
+/**
+ * @brief: 音乐音量调节
+ *
+ * @dicussion:
+ *
+ */
+- (IBAction)bgmSliderValeChange:(UISlider *)sender {
+    if(self.delegate &&[self.delegate respondsToSelector:@selector(changeMusicControlSliderValueWithPlayerUIView:bgmValue:micValue:)]){
+        // 感觉也可以直接用slider
+        [self.delegate changeMusicControlSliderValueWithPlayerUIView:self
+                                                            bgmValue:self.recordBGMValue
+                                                            micValue:self.recordMicValue];
+    }
+}
+#pragma mark -
+/**
+ * @brief:
+ *
+ * @dicussion:
+ *
+ */
+- (IBAction)micSliderValueChange:(UISlider *)sender {
+}
+#pragma mark -
+/**
+ * @brief:
+ *
+ * @dicussion:
+ *
+ */
+- (IBAction)progressSliderValueChange:(UISlider *)sender {
+}
+#pragma mark- get+Set -------------------------------------------------- get+Se方法区域 ---------------------------------------------------
+//音乐音量变化 0-1.0f
+-(void)setRecordBGMValue:(CGFloat)recordBGMValue{
+    _recordBGMValue = recordBGMValue;
+}
+//音乐麦克风变化0-1.0f
+-(void)setRecordMicValue:(CGFloat)recordMicValue{
+    _recordMicValue = recordMicValue;
+}
+//音乐进度大小变化0-1.0f
+-(void)setRecordProgressValue:(CGFloat)recordProgressValue{
+    _recordProgressValue = recordProgressValue;
 }
 @end

7de856452de95b2b454c4b514d168fc68f5e993e
岳克奎 <738816656@qq.com>
2017-01-18 23:00:15 +0800

    重新架构
    之前只是分离代码。现在才用自定义架构做到 “高聚合，低耦合”，UI组件化，最大限度减少代码

--- SuperPoweredAudioMusicPlay/ViewController.m
+++ SuperPoweredAudioMusicPlay/ViewController.m
@@ -17,23 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    
-    
-    //一般是这样的。去音乐选择界面，选择了音乐Model，把音乐的一切信息通过block回调。。加载这里，然后启动音乐的UI加载+音乐的播放
-    // 去音乐管理中心调加载音乐播放功能界面的代码
-     __weak MusicCenterManager *musicCenterManager = [MusicCenterManager shareManager];
-    [musicCenterManager showMusicPlayerPlayViewControllerOnSuperViewController:nil
-                                                                   inSuperView:nil
-                                                            musicPlayerVCFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
-                                                                      complete:^(BOOL finished,
-                                                                    MusicPlayerPlayVC *musicPlayerPlayViewController) {
-                                                                    //音乐UI加载完成后，开始启动音乐的加载
-                                                                          [musicCenterManager showSuperMusicOfSamplerateNum:MUSIC_SAMPLERATE
-                                                                                                           musicFilePathStr:@""];
-                                                                          
-                                                                                         
-    }];
 }
 
 
@@ -44,3 +27,25 @@
 
 
 @end
+
+
+
+
+
+
+
+/********* 持续更新
+    项目分析地址
+http://www.jianshu.com/p/053eafecb295
+ 
+*******/
+
+
+
+
+
+
+
+
+
+
